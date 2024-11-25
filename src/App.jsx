d2d510b878122ed6550f0713548e17100a5d4c00
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-11-25 15:21:02 +0100

    add: routes to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from "./pages/Home"
+import About from "./pages/About"
+import PostsList from "./pages/PostsList"
+import DefaultLayout from './pages/DefaultLayout'
 
 
 function App() {
 
-
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DefaultLayout />}>
+            <Route index element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/posts-list' element={<PostsList />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
