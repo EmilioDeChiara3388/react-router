ad539e616fdeab750fb0202c5ea9e5848298cb13
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-11-26 15:21:11 +0100

    add: dynamic single post

--- src/pages/SinglePost.jsx
+++ src/pages/SinglePost.jsx
@@ -1,7 +1,44 @@
+import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
+
 export default function SinglePost() {
+
+    const { slug } = useParams()
+    const [post, setPost] = useState(null)
+
+    useEffect(() => {
+        fetch(`http://127.0.0.1:3000/posts/${slug}`)
+            .then(resp => resp.json())
+            .then(data => {
+                setPost(data.data)
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    },
+        [])
+
     return (
         <>
-            <h1>Post singolo</h1>
+            {post ? (
+                <section>
+                    <div className="container text-center">
+                        <h1>{post.title}</h1>
+                        <div className="card">
+                            <div>
+                                <img src={`http://localhost:3000/imgs/posts/${post.image}`} alt="" />
+                            </div>
+                            <div>
+                                <p>{post.content}</p>
+                            </div>
+                        </div>
+                    </div >
+                </section>
+            ) : (
+                <span>Dammi un attimo...</span>
+            )
+
+            }
         </>
     )
 }
